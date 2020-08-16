3d4ebfd2b7ff844f855904e0acba779a8d4b022d
Dhaval <dhvl.ptl123@yahoo.in>
2020-08-16 12:12:48 +0530

    Intial commit for react app starter

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <header>
+                <h2>React Application</h2>
+            </header>
+        </div>
+    );
+}
+
+export default App;
